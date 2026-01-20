78c91e9d10d82df223f9988c6ae66842c5e0692a
c21-japan <y-inui@century21.group>
2026-01-21 01:51:01 +0900

    Add files via upload

--- mansion_db.js
+++ mansion_db.js
@@ -1,38 +1,14 @@
-// 旧データ（part1〜part5）は無効化
-// import { MANSION_DB_PART as PART1 } from './mansion_db_part1.js';
-// import { MANSION_DB_PART as PART2 } from './mansion_db_part2.js';
-// import { MANSION_DB_PART as PART3 } from './mansion_db_part3.js';
-// import { MANSION_DB_PART as PART4 } from './mansion_db_part4.js';
-// import { MANSION_DB_PART as PART5 } from './mansion_db_part5.js';
-
-// 新データ（part6以降）
-import { MANSION_DB_PART as PART6 } from './mansion_db_part6.js';
-import { MANSION_DB_PART as PART7 } from './mansion_db_part7.js';
-import { MANSION_DB_PART as PART8 } from './mansion_db_part8.js';
-import { MANSION_DB_PART as PART9 } from './mansion_db_part9.js';
-import { MANSION_DB_PART as PART10 } from './mansion_db_part10.js';
-import { MANSION_DB_PART as PART11 } from './mansion_db_part11.js';
-import { MANSION_DB_PART as PART12 } from './mansion_db_part12.js';
-import { MANSION_DB_PART as PART13 } from './mansion_db_part13.js';
-import { MANSION_DB_PART as PART14 } from './mansion_db_part14.js';
-import { MANSION_DB_PART as PART15 } from './mansion_db_part15.js';
-import { MANSION_DB_PART as PART16 } from './mansion_db_part16.js';
-import { MANSION_DB_PART as PART17 } from './mansion_db_part17.js';
-import { MANSION_DB_PART as PART18 } from './mansion_db_part18.js';
+import { MANSION_DB_PART as PART1 } from './mansion_db_part1.js';
+import { MANSION_DB_PART as PART2 } from './mansion_db_part2.js';
+import { MANSION_DB_PART as PART3 } from './mansion_db_part3.js';
+import { MANSION_DB_PART as PART4 } from './mansion_db_part4.js';
+import { MANSION_DB_PART as PART5 } from './mansion_db_part5.js';
 
 export const MANSION_DB = [
-...PART6,
-...PART7,
-...PART8,
-...PART9,
-...PART10,
-...PART11,
-...PART12,
-...PART13,
-...PART14,
-...PART15,
-...PART16,
-...PART17,
-...PART18
+...PART1,
+...PART2,
+...PART3,
+...PART4,
+...PART5
 ];
 export default MANSION_DB;
