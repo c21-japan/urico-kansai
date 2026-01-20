b3c2df90958b9c7522d665a471a9ad5ff37ad472
c21-japan <y-inui@century21.group>
2026-01-21 01:32:50 +0900

    すべてのマンションデータファイルを復元し、mansion_db.jsを更新

--- mansion_db.js
+++ mansion_db.js
@@ -1,11 +1,7 @@
-// 旧データ（part1〜part5）は無効化
-// import { MANSION_DB_PART as PART1 } from './mansion_db_part1.js';
-// import { MANSION_DB_PART as PART2 } from './mansion_db_part2.js';
-// import { MANSION_DB_PART as PART3 } from './mansion_db_part3.js';
-// import { MANSION_DB_PART as PART4 } from './mansion_db_part4.js';
-// import { MANSION_DB_PART as PART5 } from './mansion_db_part5.js';
-
-// 新データ（part6以降）
+// データファイルのインポート
+import { MANSION_DB_PART as PART1 } from './mansion_db_part1.js';
+import { MANSION_DB_PART as PART3 } from './mansion_db_part3.js';
+import { MANSION_DB_PART as PART5 } from './mansion_db_part5.js';
 import { MANSION_DB_PART as PART6 } from './mansion_db_part6.js';
 import { MANSION_DB_PART as PART7 } from './mansion_db_part7.js';
 import { MANSION_DB_PART as PART8 } from './mansion_db_part8.js';
@@ -19,20 +15,49 @@ import { MANSION_DB_PART as PART15 } from './mansion_db_part15.js';
 import { MANSION_DB_PART as PART16 } from './mansion_db_part16.js';
 import { MANSION_DB_PART as PART17 } from './mansion_db_part17.js';
 import { MANSION_DB_PART as PART18 } from './mansion_db_part18.js';
+import { MANSION_DB_PART as PART19 } from './mansion_db_part19.js';
+import { MANSION_DB_PART as PART20 } from './mansion_db_part20.js';
+import { MANSION_DB_PART as PART21 } from './mansion_db_part21.js';
+import { MANSION_DB_PART as PART22 } from './mansion_db_part22.js';
+import { MANSION_DB_PART as PART23 } from './mansion_db_part23.js';
+import { MANSION_DB_PART as PART24 } from './mansion_db_part24.js';
+import { MANSION_DB_PART as PART25 } from './mansion_db_part25.js';
+import { MANSION_DB_PART as PART26 } from './mansion_db_part26.js';
+import { MANSION_DB_PART as PART27 } from './mansion_db_part27.js';
+import { MANSION_DB_PART as PART28 } from './mansion_db_part28.js';
+import { MANSION_DB_PART as PART29 } from './mansion_db_part29.js';
+import { MANSION_DB_PART as PART30 } from './mansion_db_part30.js';
+import { MANSION_DB_PART as PART31 } from './mansion_db_part31.js';
 
 export const MANSION_DB = [
-...PART6,
-...PART7,
-...PART8,
-...PART9,
-...PART10,
-...PART11,
-...PART12,
-...PART13,
-...PART14,
-...PART15,
-...PART16,
-...PART17,
-...PART18
+  ...PART1,
+  ...PART3,
+  ...PART5,
+  ...PART6,
+  ...PART7,
+  ...PART8,
+  ...PART9,
+  ...PART10,
+  ...PART11,
+  ...PART12,
+  ...PART13,
+  ...PART14,
+  ...PART15,
+  ...PART16,
+  ...PART17,
+  ...PART18,
+  ...PART19,
+  ...PART20,
+  ...PART21,
+  ...PART22,
+  ...PART23,
+  ...PART24,
+  ...PART25,
+  ...PART26,
+  ...PART27,
+  ...PART28,
+  ...PART29,
+  ...PART30,
+  ...PART31
 ];
 export default MANSION_DB;
