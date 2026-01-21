3ae957cc99a3a6e1d9abbd2b0d0a94ee67b561cc
c21-japan <y-inui@century21.group>
2026-01-21 16:37:26 +0900

    マンションデータをフォルダに整理（data/mansion/に分割）

--- mansion_db.js
+++ mansion_db.js
@@ -1,9 +1,10 @@
-// データファイルのインポート
-import { MANSION_DB_PART as PART1 } from './mansion_db_part1.js';
-import { MANSION_DB_PART as PART2 } from './mansion_db_part2.js';
-import { MANSION_DB_PART as PART3 } from './mansion_db_part3.js';
-import { MANSION_DB_PART as PART4 } from './mansion_db_part4.js';
-import { MANSION_DB_PART as PART5 } from './mansion_db_part5.js';
+// マンションデータベース
+// フォルダ内のデータファイルをインポート
+import { MANSION_DB_PART as PART1 } from './data/mansion/part1.js';
+import { MANSION_DB_PART as PART2 } from './data/mansion/part2.js';
+import { MANSION_DB_PART as PART3 } from './data/mansion/part3.js';
+import { MANSION_DB_PART as PART4 } from './data/mansion/part4.js';
+import { MANSION_DB_PART as PART5 } from './data/mansion/part5.js';
 
 export const MANSION_DB = [
   ...PART1,
@@ -12,4 +13,5 @@ export const MANSION_DB = [
   ...PART4,
   ...PART5
 ];
+
 export default MANSION_DB;
